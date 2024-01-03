2a0cc16a8bf09ca302b198cd7d3c418d7e15deb6
Kyryh <kyrykiller07@gmail.com>
2024-01-03 11:37:09 +0100

    Made an extension method public
    it was private because i'm stuypid

--- Scripts/ExtensionMethods.cs
+++ Scripts/ExtensionMethods.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace ReturnsArtifacts.Scripts {
     internal static class ExtensionMethods {
-        private static IEnumerable<Type> GetTypesOfType<T>(this Assembly assembly) {
+        public static IEnumerable<Type> GetTypesOfType<T>(this Assembly assembly) {
             return assembly.GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(T)));
         }
     }
