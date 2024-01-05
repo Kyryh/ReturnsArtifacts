1ae224e111fbb7bd08e0e6acd092b9b9882f9b4d
Kyryh <kyrykiller07@gmail.com>
2024-01-05 14:40:47 +0100

    Update ArtifactOfPrestige.cs

--- Scripts/Artifacts/ArtifactOfPrestige.cs
+++ Scripts/Artifacts/ArtifactOfPrestige.cs
@@ -4,10 +4,12 @@ using UnityEngine.Networking;
 using RoR2;
 using static ReturnsArtifacts.Scripts.Plugin;
 using R2API;
+using R2API.Utils;
+using R2API.AddressReferencedAssets;
 using System;
 using System.Runtime;
-using R2API.Utils;
 using Newtonsoft.Json.Utilities;
+using UnityEngine.AddressableAssets;
 
 namespace ReturnsArtifacts.Scripts.Artifacts {
     class ArtifactOfPrestige : ArtifactBase {
@@ -17,9 +19,12 @@ namespace ReturnsArtifacts.Scripts.Artifacts {
         public override Sprite ArtifactEnabledIcon => Assets.LoadAsset<Sprite>("ArtifactOfPrestigeEnabled.png");
         public override Sprite ArtifactDisabledIcon => Assets.LoadAsset<Sprite>("ArtifactOfPrestigeDisabled.png");
 
+        private SpawnCard mountainShrineSpawnCard = Addressables.LoadAssetAsync<SpawnCard>("RoR2/Base/ShrineBoss/iscShrineBoss.asset").WaitForCompletion();
 
 
+        private readonly static Color shrineSymbolColor = new Color(0.8f, 0.2f, 0.8f);
 
+        private static int shrineStacks;
         public override void Init() {
             CreateLang();
             CreateArtifact();
@@ -29,13 +34,32 @@ namespace ReturnsArtifacts.Scripts.Artifacts {
         public override void Hooks() {
             SceneDirector.onGenerateInteractableCardSelection += SpawnMountainShrine;
             SpawnCard.onSpawnedServerGlobal += ConvertSpawnedMountainShrines;
+            // Set the shrine stacks to 0 on run start
+            Run.onRunStartGlobal += (_) => {
+                LogDebug("resetting shrine stacks");
+                shrineStacks = 0;
+            };
+            // Save the teleporter shrine stacks
+            Stage.onServerStageComplete += (_) => {
+                LogDebug($"setting shrinestacks to {TeleporterInteraction.instance.shrineBonusStacks}");
+                shrineStacks = TeleporterInteraction.instance.shrineBonusStacks;
+            };
+            // Load the previous teleporter shrine stacks
+            Stage.onServerStageBegin += (_) => {
+                LogDebug($"setting teleporter shrinestacks to {shrineStacks}");
+                for (int i = 0; i < shrineStacks; i++) {
+                    TeleporterInteraction.instance.AddShrineStack();
+                }
+                
+            };
+
         }
 
         private void ConvertSpawnedMountainShrines(SpawnCard.SpawnResult result) {
             if (result.success) {
                 if (result.spawnRequest.spawnCard.IsMountainShrine()) {
                     GameObject shineOfTheMountain = result.spawnedInstance;
-                    shineOfTheMountain.transform.Find("Symbol").GetComponent<MeshRenderer>().sharedMaterial.SetColor("_TintColor", Color.magenta);
+                    shineOfTheMountain.transform.Find("Symbol").GetComponent<MeshRenderer>().sharedMaterial.SetColor("_TintColor", shrineSymbolColor);
                     shineOfTheMountain.GetComponent<PurchaseInteraction>().setUnavailableOnTeleporterActivated = false;
                 }
             }
