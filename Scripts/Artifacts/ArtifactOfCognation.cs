5811728afb026d0a22516405e35737f7b67ac02d
Kyryh <kyrykiller07@gmail.com>
2024-01-09 22:54:00 +0100

    Fixed ghost jellyfishes not dying
    another check to the bajillion checks woohoo!!!!

--- Scripts/Artifacts/ArtifactOfCognation.cs
+++ Scripts/Artifacts/ArtifactOfCognation.cs
@@ -36,7 +36,9 @@ namespace ReturnsArtifacts.Scripts.Artifacts {
 
         private void SpawnMonsterClone(DamageReport damageReport) {
             // a bajillion checks 
-            if (NetworkClient.active && ArtifactEnabled && damageReport != null && damageReport.victimBody && damageReport.victimTeamIndex != TeamIndex.Player) {
+            if (NetworkClient.active && ArtifactEnabled && damageReport != null &&
+                damageReport.victimBody && damageReport.victimTeamIndex != TeamIndex.Player &&
+                damageReport.victimBody.inventory.GetItemCount(RoR2Content.Items.Ghost) != 0) {
                 CharacterBody charBody = Util.TryToCreateGhost(damageReport.victimBody, damageReport.victimBody, cloneLifespan.Value);
                 // remove the "BoostDamage" item because otherwise
                 // the clones fucking demolish you
